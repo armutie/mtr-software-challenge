53d34662934652bb3e5f29749a010d026915bf8d
Abdur Rahman Mutie <abdurrahmanmutie14@gmail.com>
2026-09-13 13:22:39 -0400

    Publish visualization frame for RViz

--- src/mtr_software_challenge/launch/challenge.launch.py
+++ src/mtr_software_challenge/launch/challenge.launch.py
@@ -8,8 +8,15 @@ from launch_ros.actions import Node
 def generate_launch_description():
     config = Path(get_package_share_directory("mtr_software_challenge")) / "config/challenge.rviz"
     return LaunchDescription([
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=[
+                "--frame-id", "map",
+                "--child-frame-id", "visualization_anchor",
+            ],
+        ),
         Node(package="mtr_software_challenge", executable="simulator", output="screen"),
         Node(package="mtr_software_challenge", executable="controller", output="screen"),
         Node(package="rviz2", executable="rviz2", arguments=["-d", str(config)]),
     ])
-
